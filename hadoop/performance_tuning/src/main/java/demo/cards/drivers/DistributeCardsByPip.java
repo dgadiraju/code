81471161cdc21f196e074eb5410fd8cd961de3e4
DurgaGadiraju <dgadiraju@gmail.com>
2014-12-30 12:49:51 -0600

    Added new files to performance tuning

---
+++ hadoop/performance_tuning/src/main/java/demo/cards/drivers/DistributeCardsByPip.java
@@ -0,0 +1,85 @@
+package demo.cards.drivers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cloudera
+ * Date: 9/22/13
+ * Time: 1:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import demo.cards.parsers.CardParser;
+
+import java.io.IOException;
+
+public class DistributeCardsByPip extends Configured implements Tool {
+
+    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+        private final static IntWritable one = new IntWritable(1);
+        private CardParser parser = new CardParser();
+        private String pip;
+
+        public void map(LongWritable key, Text value, Context output) throws IOException, InterruptedException {
+            parser.parse(value.toString());
+            pip = parser.getPip();
+            output.write(new Text(pip), value);
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, Text, NullWritable, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context output) throws IOException, InterruptedException {
+            while (values.iterator().hasNext()) {
+                output.write(NullWritable.get(), values.iterator().next());
+            }
+        }
+    }
+
+    public int run(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Job job = Job.getInstance(getConf(), "Distribute by Pip in deck of cards");
+
+        job.setJarByClass(getClass());
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setMapperClass(Map.class);
+//        job.setGroupingComparatorClass(TextPair.Comparator.class);
+//        job.setSortComparatorClass();
+//        job.setCombinerClass(Reduce.class);
+        job.setReducerClass(Reduce.class);
+
+//        job.setNumReduceTasks(2);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new DistributeCardsByPip(), args);
+        System.exit(exitCode);
+
+    }
+}
---
+++ hadoop/performance_tuning/src/main/java/demo/cards/drivers/RowCount.java
@@ -0,0 +1,87 @@
+package demo.cards.drivers;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+//import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+//import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import demo.cards.mappers.RecordMapper;
+import demo.cards.reducers.NoKeyRecordCountReducer;
+
+/* This program can be used for 
+ * select count(1) from <table_name>;
+ * Steps
+ * 1. Develop mapper function
+ * 2. Develop reducer function
+ * 3. Job configuration
+ */
+
+public class RowCount extends Configured implements Tool {
+
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new RowCount(), args);
+		System.exit(exitCode);
+
+	}
+
+	public int run(String[] args) throws Exception {
+
+		Configuration conf = getConf();
+
+		conf.set("mapreduce.input.fileinputformat.split.minsize", "256000000");
+		Job job = Job.getInstance(conf,
+				"Row Count using built in mappers and reducers");
+
+		job.setJarByClass(getClass());
+
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		// We are not setting input format class and hence uses
+		// (TextInputFormat)
+		// job.setInputFormatClass(TextInputFormat.class);
+
+		// Custom mapper (RecordMapper) to assign 1 for each record
+		// Input to mapper <Lineoffset as key, entire line as value>
+		// Default behavior of default input format TextInputFormat
+		job.setMapperClass(RecordMapper.class);
+		// Output from mapper
+		// <count, 1>
+		// <count, 1> so on
+
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+
+		// Built-in reducer
+		// Input to reducer <count, {1, 1, 1, ...}>
+		// job.setReducerClass(IntSumReducer.class);
+		// Output from reducer <count, Number of records>
+
+		// Custom reducer
+		// If you do not want to see "count" as part of output
+		// and just see the record count as in select count query
+		// job.setCombinerClass(NoKeyRecordCountReducer.class);
+		job.setReducerClass(NoKeyRecordCountReducer.class);
+		// Output from reducer <Number of records>
+
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// We are not setting output format class and hence uses default
+		// (TextOutputFormat)
+
+		// job.setOutputFormatClass(TextOutputFormat.class);
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+}

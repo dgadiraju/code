957b4fcef607884c6fa93efd206370e5d2160293
DurgaGadiraju <dgadiraju@gmail.com>
2015-01-12 20:12:07 -0600

    Custom Partitioner

--- hadoop/performance_tuning/src/main/java/demo/cards/drivers/DistributeCardsByPip.java
+++ hadoop/performance_tuning/src/main/java/demo/cards/drivers/DistributeCardsByPip.java
@@ -10,7 +10,6 @@ package demo.cards.drivers;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -20,6 +19,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -28,58 +28,74 @@ import demo.cards.parsers.CardParser;
 
 import java.io.IOException;
 
+// This map reduce program will copy data partitioned by pip
 public class DistributeCardsByPip extends Configured implements Tool {
 
-    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-        private final static IntWritable one = new IntWritable(1);
-        private CardParser parser = new CardParser();
-        private String pip;
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+		private CardParser parser = new CardParser();
+		private String pip;
 
-        public void map(LongWritable key, Text value, Context output) throws IOException, InterruptedException {
-            parser.parse(value.toString());
-            pip = parser.getPip();
-            output.write(new Text(pip), value);
-        }
-    }
+		public void map(LongWritable key, Text value, Context output)
+				throws IOException, InterruptedException {
+			parser.parse(value.toString());
+			pip = parser.getPip();
+			output.write(new Text(pip), new Text(parser.getColorAndSuit()));
+		}
+	}
 
-    public static class Reduce extends Reducer<Text, Text, NullWritable, Text> {
-        public void reduce(Text key, Iterable<Text> values, Context output) throws IOException, InterruptedException {
-            while (values.iterator().hasNext()) {
-                output.write(NullWritable.get(), values.iterator().next());
-            }
-        }
-    }
+	public static class Reduce extends Reducer<Text, Text, NullWritable, Text> {
 
-    public int run(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
-        Job job = Job.getInstance(getConf(), "Distribute by Pip in deck of cards");
+		private MultipleOutputs<NullWritable, Text> multipleOutputs;
 
-        job.setJarByClass(getClass());
+		protected void setup(Context context) throws IOException,
+				InterruptedException {
+			multipleOutputs = new MultipleOutputs<NullWritable, Text>(context);
 
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
-        job.setOutputKeyClass(NullWritable.class);
-        job.setOutputValueClass(Text.class);
+		}
 
-        job.setMapperClass(Map.class);
-//        job.setGroupingComparatorClass(TextPair.Comparator.class);
-//        job.setSortComparatorClass();
-//        job.setCombinerClass(Reduce.class);
-        job.setReducerClass(Reduce.class);
+		public void reduce(Text key, Iterable<Text> values, Context output)
+				throws IOException, InterruptedException {
+			while (values.iterator().hasNext()) {
+				String basePath = String.format("%s/part",
+						"pip=" + key.toString());
+				multipleOutputs.write(NullWritable.get(), values.iterator()
+						.next(), basePath);
+			}
+		}
+	}
 
-//        job.setNumReduceTasks(2);
+	public int run(String[] args) throws IOException, InterruptedException,
+			ClassNotFoundException {
+		Job job = Job.getInstance(getConf(),
+				"Distribute by Pip in deck of cards");
 
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
+		job.setJarByClass(getClass());
 
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(Text.class);
 
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
+		job.setMapperClass(Map.class);
+		// job.setGroupingComparatorClass(TextPair.Comparator.class);
+		// job.setSortComparatorClass();
+		// job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
 
-    public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new DistributeCardsByPip(), args);
-        System.exit(exitCode);
+		job.setNumReduceTasks(4);
 
-    }
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new DistributeCardsByPip(), args);
+		System.exit(exitCode);
+
+	}
 }
--- hadoop/performance_tuning/src/main/java/demo/cards/parsers/CardParser.java
+++ hadoop/performance_tuning/src/main/java/demo/cards/parsers/CardParser.java
@@ -1,46 +1,47 @@
 package demo.cards.parsers;
 
 /**
- * Created with IntelliJ IDEA.
- * User: kavithapenmetsa
- * Date: 10/20/13
- * Time: 11:06 AM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: kavithapenmetsa Date: 10/20/13 Time: 11:06
+ * AM To change this template use File | Settings | File Templates.
  */
 public class CardParser {
-    private String color;
-    private String suit;
-    private String pip;
+	private String color;
+	private String suit;
+	private String pip;
 
-    public void parse(String record) {
-        String[] vals = record.split("\\|");
-        color = vals[0];
-        suit = vals[1];
-        pip = vals[2];
-    }
+	public void parse(String record) {
+		String[] vals = record.split("\\|");
+		color = vals[0];
+		suit = vals[1];
+		pip = vals[2];
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    public String getSuit() {
-        return suit;
-    }
+	public String getSuit() {
+		return suit;
+	}
 
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
 
-    public String getPip() {
-        return pip;
-    }
+	public String getPip() {
+		return pip;
+	}
 
-    public void setPip(String pip) {
-        this.pip = pip;
-    }
+	public void setPip(String pip) {
+		this.pip = pip;
+	}
+
+	public String getColorAndSuit() {
+		return color + "|" + suit;
+	}
 
 }
